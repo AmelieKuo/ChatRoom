e4fdadd58ec7534e48c3dbe853c04e3aaa75f7ed
AmelieKuo <amelie.kkuo@gmail.com>
2024-11-01 00:08:54 +0800

    feat: 修改 line api

--- api/line.ts
+++ api/line.ts
@@ -4,8 +4,8 @@ const lineUrl = 'https://api.line.me/v2'
 
 export const FETCH_LINE = {
   GetToken: (data: any, headers: object = {}, isUnLoad: boolean = true) =>
-    request.post('/token', data, headers, isUnLoad),
+    request.post('/token', data, lineUrl, headers, isUnLoad),
 
   GetProfile: (data: any, headers: object = {}, isUnLoad: boolean = true) =>
-    request.get('/users', data, headers, isUnLoad),
+    request.get('/users', data, lineUrl, headers, isUnLoad),
 }
